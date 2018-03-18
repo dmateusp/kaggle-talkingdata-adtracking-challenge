f4e12df3e51edb705eef273d0a5abee3b898c0d5
Daniel Pires <dmateusp@gmail.com>
2018-03-18 21:37:29 +0000

    adding notebooks from kaggle and jupyter configuration file

---
+++ jupyter_notebook_config.py
@@ -0,0 +1,8 @@
+# Configuration file for jupyter-notebook.
+
+## (bytes/sec) Maximum rate at which messages can be sent on iopub before they
+#  are limited.
+c.NotebookApp.iopub_data_rate_limit = 10000000
+
+## The directory to use for notebooks and kernels.
+c.NotebookApp.notebook_dir = u'./notebooks'
